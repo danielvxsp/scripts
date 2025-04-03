8053528e857489d1139d9aa3a87c566048c1d712
XsPDevKX <96445862+XsPDevKX@users.noreply.github.com>
2025-04-03 19:09:28 +0200

    add retardation

---
+++ slstatuscheck.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# this is so retarded 
+# Fixing issue with XOpenDisplay
+# Set the X11 display 
+export DISPLAY=:0
+export XAUTHORITY=/home/$(users)/.Xauthority  
+# set D-bus variables
+export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
+
+# Check if slstatus is running
+if ! pgrep -x "slstatus" > /dev/null; then
+    echo "slstatus not running. starting..."
+    /usr/local/bin/slstatus &
+else
+    echo "slstatus is running"
+fi
